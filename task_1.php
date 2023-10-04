744794ae87da833005576314149ad5421906134d
Zaki Mahmud <mahmudzaki7@gmail.com>
2023-10-04 23:42:10 +0600

    Add files via upload

---
+++ task_1.php
@@ -0,0 +1,30 @@
+<?php
+
+class Product {
+    public $id;
+    public $name;
+    public $price;
+
+    public function __construct($id, $name, $price) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    public function getFormattedPrice() {
+        return '$' . number_format($this->price, 2);
+    }
+
+    public function showDetails() {
+        echo "Product Details:\n";
+        echo "- ID: {$this->id}\n";
+        echo "- Name: {$this->name}\n";
+        echo "- Price: {$this->getFormattedPrice()}\n";
+    }
+}
+
+// Test the Product class
+$product = new Product(1, 'T-shirt', 19.99);
+$product->showDetails();
+
+?>
